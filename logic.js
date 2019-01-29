5161ade64a087aadc67839ead34046a0a20acc82
kaushikDev <piyush.kaushik02@gmail.com>
2019-01-29 23:53:41 +0530

    jhgj

--- logic.js
+++ logic.js
@@ -93,9 +93,9 @@ firebase.auth().signInWithRedirect(provider);
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		
 	if(googleAuth){
 		 googleAuth.addEventListener('click', e=>{
-			firebase.auth().signInWithRedirect(provider);
-			firebase.auth().getRedirectResult().then(function(result) {
-			//firebase.auth().signInWithPopup(provider).then(function(result) {	 
+			//firebase.auth().signInWithRedirect(provider);
+			//firebase.auth().getRedirectResult().then(function(result) {
+			firebase.auth().signInWithPopup(provider).then(function(result) {	 
 			// This gives you a Google Access Token. You can use it to access the Google API.
   			var token = result.credential.accessToken;
 			// The signed-in user info.
