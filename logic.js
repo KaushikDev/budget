4f606aea4906a2de0e1f08eb1555b93e46e3d8ba
kaushikDev <piyush.kaushik02@gmail.com>
2019-01-29 23:47:02 +0530

    Secons commit

--- logic.js
+++ logic.js
@@ -12,6 +12,10 @@
 //===============================================================================================
 $("document").ready(function(){
 
+
+
+const googleAuth = document.getElementById("googleAuth");
+
 const budgetAmount = document.getElementById("budgetAmount");
 const expenseAmount = document.getElementById("expenseAmount");	
 const balanceAmount = document.getElementById("balanceAmount");	
@@ -26,6 +30,9 @@ var databaseRef = firebase.database().ref();
 
 var totalExpenseAmount = 0;	
 var totalBalanceAmount = 0;
+
+var provider = new firebase.auth.GoogleAuthProvider();
+firebase.auth().signInWithRedirect(provider);
  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++	
 	initApp();
  //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -83,7 +90,32 @@ var totalBalanceAmount = 0;
 
     
 	//+++++++++++++++ budgetButton ++++++++++++++++++++++    
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		
+	if(googleAuth){
+		 googleAuth.addEventListener('click', e=>{
+			//firebase.auth().signInWithRedirect(provider);
+			//firebase.auth().getRedirectResult().then(function(result) {
+			firebase.auth().signInWithPopup(provider).then(function(result) {	 
+			// This gives you a Google Access Token. You can use it to access the Google API.
+  			var token = result.credential.accessToken;
+			// The signed-in user info.
+			  var user = result.user;
+			
+			}).catch(function(error) {
+ 			 // Handle Errors here.
+ 			 var errorCode = error.code;
+			  var errorMessage = error.message;
+			  // The email of the user's account used.
+			  var email = error.email;
+ 			 // The firebase.auth.AuthCredential type that was used.
+  			var credential = error.credential;
+  			// ...
+			});
+		 });
+		
+		}
+	
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		
 		
 
